0cef62918bd723c68647e40dc9843da34b5911ad
Mena Ehab <127118141+menaehab@users.noreply.github.com>
2025-07-18 09:31:00 +0300

    chore: install and configure Laravel Reverb

--- app/Livewire/Home/RoomList.php
+++ app/Livewire/Home/RoomList.php
@@ -2,12 +2,14 @@
 
 namespace App\Livewire\Home;
 
+use App\Models\Room;
 use Livewire\Component;
 
 class RoomList extends Component
 {
     public function render()
     {
-        return view('livewire.home.room-list');
+        $rooms = Room::all();
+        return view('livewire.home.room-list',compact('rooms'));
     }
 }
--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,11 +2,26 @@
 
 namespace App\Models;
 
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Model;
 
 class Room extends Model
 {
+    use HasSlug;
     protected $guarded = ['id'];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
 
     public function topic()
     {
