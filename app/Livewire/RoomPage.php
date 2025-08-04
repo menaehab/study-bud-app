26ee4124c06938a9683f19c6f6061c1a4eed04e4
Mena Ehab <commanderyt123@gmail.com>
2025-08-04 16:41:00 +0300

    feat: send message

--- app/Livewire/RoomPage.php
+++ app/Livewire/RoomPage.php
@@ -2,10 +2,43 @@
 
 namespace App\Livewire;
 
+use App\Models\Room;
+use App\Models\Message;
 use Livewire\Component;
 
 class RoomPage extends Component
 {
+    public $room;
+    public $messages;
+    public $message;
+    public $users;
+    public function mount($slug)
+    {
+        $this->room = Room::where('slug', $slug)->with('messages.user')->first();
+        $this->messages = $this->room->messages->all();
+        // Get all users in the room + user owner of the room
+        $this->users = $this->room->messages->pluck('user')->merge(collect([$this->room->user]))->unique()->all();
+    }
+    public function rules(): array
+    {
+        return [
+            'message' => 'required|string|max:255',
+        ];
+    }
+
+    public function sendMessage()
+    {
+        $this->validate($this->rules());
+
+        Message::create([
+            'message' => $this->message,
+            'room_id' => $this->room->id,
+            'user_id' => auth()->id(),
+        ]);
+
+        $this->message = '';
+    }
+
     public function render()
     {
         return view('livewire.room-page');
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Livewire\Volt\Volt;
 use App\Livewire\HomePage;
+use App\Livewire\RoomPage;
 use App\Events\RefreshRooms;
 use App\Livewire\RoomCreate;
 use App\Livewire\TopicsPage;
@@ -9,15 +10,22 @@ use App\Livewire\ProfilePage;
 use App\Livewire\SettingsPage;
 use Illuminate\Support\Facades\Route;
 
+# Home
 Route::get('/', HomePage::class)->name('home');
 
-
+# Must be Authenticated
 Route::middleware(['auth', 'verified'])->group(function () {
+    # Create Room
     Route::get('/create-room', RoomCreate::class)->name('room.create');
-    Route::get('/settings', SettingsPage::class)->name('settings');
-    Route::get('/profile/{slug}', ProfilePage::class)->name('profile');
+    # Room
+    Route::get('/rooms/{slug}', RoomPage::class)->name('room');
+    # Topics
     Route::get('/topics', TopicsPage::class)->name('topics');
+    # Profile
+    Route::get('/profile/{slug}', ProfilePage::class)->name('profile');
+    # Settings
+    Route::get('/settings', SettingsPage::class)->name('settings');
 });
 
-
+# Auth
 require __DIR__ . '/auth.php';
