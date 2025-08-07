38a9b5fb8519af4013708d992a101ebb1aaecb4e
Mena Ehab <commanderyt123@gmail.com>
2025-08-07 12:30:55 +0300

    feat: realtime send messages

--- app/Livewire/RoomPage.php
+++ app/Livewire/RoomPage.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\Room;
 use App\Models\Message;
 use Livewire\Component;
+use App\Events\MessageRefreshed;
 
 class RoomPage extends Component
 {
@@ -12,23 +13,33 @@ class RoomPage extends Component
     public $messages;
     public $message;
     public $users;
+    public $onlineUsers;
+    public $usersCount;
+    protected function getListeners()
+    {
+        return [
+            "echo-presence:room.{$this->room->id},.refresh-messages" => 'loadMessages',
+        ];
+    }
+
     public function mount($slug)
     {
         $this->room = Room::where('slug', $slug)->with('messages.user')->first();
-        $this->messages = $this->room->messages->all();
-        // Get all users in the room + user owner of the room
-        $this->users = $this->room->messages->pluck('user')->merge(collect([$this->room->user]))->unique()->all();
+        $this->loadMessages();
     }
-    public function rules(): array
+
+    public function loadMessages()
     {
-        return [
-            'message' => 'required|string|max:255',
-        ];
+        $this->messages = $this->room->messages->all();
+        $this->users = $this->room->messages->pluck('user')->unique()->all();
+        $this->usersCount = count($this->users);
     }
 
     public function sendMessage()
     {
-        $this->validate($this->rules());
+        $this->validate([
+            'message' => 'required|string|max:255',
+        ]);
 
         Message::create([
             'message' => $this->message,
@@ -36,7 +47,35 @@ class RoomPage extends Component
             'user_id' => auth()->id(),
         ]);
 
+        event(new MessageRefreshed($this->room->id));
+
         $this->message = '';
+        $this->loadMessages();
+    }
+
+    public function deleteMessage($id)
+    {
+        $message = Message::find($id);
+        if ($message->user_id != auth()->id()) {
+            return;
+        }
+
+        $message->delete();
+
+        event(new MessageRefreshed($this->room->id));
+
+        $this->loadMessages();
+    }
+
+    public function deleteRoom($id)
+    {
+        $room = Room::find($id);
+        if ($room->user_id != auth()->id()) {
+            return;
+        }
+
+        $room->delete();
+        $this->redirect(route('home'), navigate: true);
     }
 
     public function render()
--- routes/channels.php
+++ routes/channels.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return true;
 });
+
+Broadcast::channel('room.{roomId}', function ($user, $roomId) {
+    if (!$user) {
+        return false;
+    }
+
+
+    return [
+        'id' => $user->id,
+        'name' => $user->name,
+    ];
+});
